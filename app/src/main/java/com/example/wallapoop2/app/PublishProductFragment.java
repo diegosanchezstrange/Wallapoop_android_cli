f6e0bb7705be34853ad174c5ad359a1fe5b774c9
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2020-02-26 17:46:27 +0100

    App now sends product image

--- app/src/main/java/com/example/wallapoop2/app/PublishProductFragment.java
+++ app/src/main/java/com/example/wallapoop2/app/PublishProductFragment.java
@@ -2,22 +2,39 @@ package com.example.wallapoop2.app;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.wallapoop2.MainActivity;
 import com.example.wallapoop2.R;
 import com.example.wallapoop2.product.Product;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 
 
 /**
@@ -26,7 +43,9 @@ import com.google.android.material.textfield.TextInputEditText;
 public class PublishProductFragment extends Fragment
 {
 
+    private String publishURL = "http://192.168.0.16:5001/products";
     private ImageView image;
+    private Bitmap imageBitMap;
 
 
     public PublishProductFragment() {
@@ -64,10 +83,43 @@ public class PublishProductFragment extends Fragment
             @Override
             public void onClick(View v)
             {
-                publishProduct(name, price, desc, image);
-
                 // TODO
                 // Recoger el producto y mandarlo al servidor
+                HashMap<String, String> params = new HashMap<>();
+
+                Integer product_owner = 1;
+
+                params.put("nombre", name.getText().toString());
+                params.put("descripcion", desc.getText().toString());
+                params.put("precio", price.getText().toString());
+                params.put("product_owner", product_owner.toString());
+                params.put("image", imageToString(imageBitMap));
+
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, publishURL, new JSONObject(params),
+                                new Response.Listener<JSONObject>()
+                                {
+                                    @Override
+                                    public void onResponse(JSONObject response)
+                                    {
+                                        try {
+                                            Toast.makeText(getActivity(), response.getString("message"), Toast.LENGTH_LONG);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG);
+                            }
+                        });
+
+                RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+                requestQueue.add(jsonObjectRequest);
 
             }
         });
@@ -91,10 +143,25 @@ public class PublishProductFragment extends Fragment
                     // Almacenar la foto en la base de datos y obtenerla desde ahí para que sea persistente(?)
 
                     image.setImageURI(selectedImage);
+                    try
+                    {
+                        imageBitMap = MediaStore.Images.Media.getBitmap(this.getActivity().getContentResolver(), selectedImage);
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
 
                     break;
             }
         }
+    }
+
+    private String imageToString(Bitmap image)
+    {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] imgBytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
     }
 
 
