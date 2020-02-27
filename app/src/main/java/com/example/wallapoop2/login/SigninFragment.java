ee1a994f5981c83ff962917ef8f9b74d49c0ca2d
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2020-02-27 02:48:37 +0100

    Signin works

--- app/src/main/java/com/example/wallapoop2/login/SigninFragment.java
+++ app/src/main/java/com/example/wallapoop2/login/SigninFragment.java
@@ -1,6 +1,7 @@
 package com.example.wallapoop2.login;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,8 +12,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.wallapoop2.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 
 /**
@@ -22,6 +36,7 @@ public class SigninFragment extends Fragment
 {
 
     private NavController myNavCtrl;
+    private String registerURL;
 
     public SigninFragment() {
         // Required empty public constructor
@@ -31,18 +46,61 @@ public class SigninFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_signin, container, false);
+        final View v = inflater.inflate(R.layout.fragment_signin, container, false);
+
+        registerURL = getString(R.string.server_url) + ":5000/register";
 
         myNavCtrl = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
 
         Button signinButton = v.findViewById(R.id.buttonSigning);
 
+        final EditText name = v.findViewById(R.id.userSigninName);
+        final EditText passwd = v.findViewById(R.id.userSigninPasswd);
+        final EditText passwdRepeat = v.findViewById(R.id.userSigningPasswdRepeat);
+
         signinButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v)
+            public void onClick(final View v)
             {
-                myNavCtrl.navigate(R.id.actionSigninToSplash);
+                String passwdStr = passwd.getText().toString();
+                String passwdRepeatStr = passwdRepeat.getText().toString();
+                if(passwdStr.equals(passwdRepeatStr))
+                {
+                    HashMap<String, String> loginParams = new HashMap<String, String>();
+
+                    loginParams.put("name", name.getText().toString());
+                    loginParams.put("passwd", passwd.getText().toString());
+
+
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                            (Request.Method.POST, registerURL, new JSONObject(loginParams),
+                                    new Response.Listener<JSONObject>()
+                                    {
+                                        @Override
+                                        public void onResponse(JSONObject response)
+                                        {
+                                            Snackbar.make(v, "Usuario registrado.", Snackbar.LENGTH_SHORT).show();
+
+                                            myNavCtrl.navigate(R.id.actionSigninToSplash);
+
+                                        }
+                                    }, new Response.ErrorListener()
+                            {
+                                @Override
+                                public void onErrorResponse(VolleyError error)
+                                {
+
+                                }
+                            });
+
+                    RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+                    requestQueue.add(jsonObjectRequest);
+                }else
+                {
+                    Snackbar.make(v, "Las claves no coinciden.", Snackbar.LENGTH_SHORT).show();
+                }
+
             }
         });
 
